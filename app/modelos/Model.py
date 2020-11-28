9c0770f9aa7aacedad47b6f6cadaa67d59c74ea3
JoseCcari <38201833+JoseCcari@users.noreply.github.com>
2020-11-28 18:23:11 -0500

    Update Model.py

--- app/modelos/Model.py
+++ app/modelos/Model.py
@@ -217,4 +217,77 @@ class Model:
         self.cursor.execute(query)
         self.con.commit()
         return "Insert Succesful"
+    
+     #Agregar Docente
+    def addDocente(self):
+        print(request.json)
+        query = f'INSERT INTO Docente (doc_dni , doc_nom , doc_ape_pat , doc_ape_mat , doc_grad_aca , dep_aca_ide ) VALUES ({request.json["dni"]},"{request.json["name"]}","{request.json["lastname1"]}","{request.json["lastname2"]}","{request.json["gradoacademico"]}",{request.json["depAcademico"]})'
+        print (query)
+        self.cursor.execute(query)
+        self.con.commit()
+        return "Insert Succesful"
+
+    #Buscar Docente
+    def searchDocente(self, dni):
+        Docentes = []
+        query = "SELECT doc_dni , doc_nom , doc_ape_pat , doc_ape_mat , doc_grad_aca , dep_aca_ide FROM Docente WHERE doc_dni = %s AND doc_del_date is null "
+        self.cursor.execute(query, (dni,))
+        data = self.cursor.fetchall()
+
+        for dnis in data:
+            s = {}
+            s["doc_dni"] = dnis[0]
+            s["doc_nom"] = dnis[1]
+            s["doc_ape_mat"] = dnis[2]
+            s["doc_ape_pat"] = dnis[3]
+            s["doc_grad_aca"] = dnis[4]
+            s["dep_aca_ide"] = dnis[5]
+            Docentes.append(s)
+
+        return jsonify(Docentes)
+
+    #Borrar Docente
+    def deleteDocente(self,id):
+        query = "UPDATE Docente SET doc_del_date=now() WHERE doc_ide= %s"
+        self.cursor.execute(query, (id,))
+        self.con.commit()
+        return "Docente Eliminado"
+
+    # Actualizar Docente
+    def updateDocente(self ):
+        newdata = request.json
+        t = newdata["doc_ide"]
+        
+        query = "UPDATE Docente SET doc_dni = %s , doc_nom = %s, doc_ape_pat = %s, doc_ape_mat = %s, doc_grad_aca = %s, dep_aca_ide = %s WHERE doc_ide = %s" 
+
+        print (query)
+    
+        self.cursor.execute(query,( newdata["doc_dni"],newdata["doc_nom"] ,newdata["doc_ape_pat"],newdata["doc_ape_mat"],newdata["doc_grad_aca"],newdata["dep_aca_ide"],newdata["doc_ide"]))
+        self.con.commit()
+        return "Docente Actualizado"
+        
+  
+    # Buscar Curso 
+    def searchCurs(self, cod):
+        Cursos = []
+        query = "SELECT cur_cod , cur_nom , cur_sem , cur_dur , cur_hor_teo , cur_hor_prac , cur_hor_lab , cur_credi , cur_fund FROM Curso WHERE cur_cod = %s "
+        
+        self.cursor.execute(query, (cod,))
+        data = self.cursor.fetchall()
+
+        for codi in data:
+            s = {}
+            s["cur_cod"] = codi[0]
+            s["cur_nom"] = codi[1]
+            s["cur_sem"] = codi[2]
+            s["cur_dur"] = codi[3]
+            s["cur_hor_teo"] = codi[4]
+            s["cur_hor_prac"] = codi[5]
+            s["cur_hor_lab"] = codi[6]
+            s["cur_credi"] = codi[7]
+            s["cur_fund"] = codi[8]
+            
+            Cursos.append(s)
+
+        return jsonify(Cursos)
 ###### End Rodrigo ############ End Rodrigo ############ End Rodrigo ############  End Rodrigo ############ End Rodrigo ######
